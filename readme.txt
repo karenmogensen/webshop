73b0eb4faab1c8b8dcce9842308473492b4c65f4
karenmogensen <post@karenmogensen>
2015-02-14 14:33:17 +0100

    installed dependencies

---
+++ readme.txt
@@ -0,0 +1,27 @@
+# webshop
+## Synopsis
+
+This project is used in angularJS education. The project is a simple webshop application.
+
+## Code Example
+
+
+## Motivation
+
+## Installation
+
+## API Reference
+
+AngularJS
+JQuery
+Bootstrap
+
+## Tests
+
+## Contributors
+
+Karen Mogensen
+
+## License
+
+Karen Mogensen
---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+port = process.argv[2] || 8001;
+app.configure( function (){
+	app.use(
+		"/",
+		express.static(__dirname)
+	);
+});
+app.listen(port); //the port you want to use
+console.log("Express server running at => http://localhost:" + port + "/\nCTRL + C to shutdown");
